44c2d0744d5bf67c14cad373a9c8714a41751019
Rafael Vastag <rgvastag@gmail.com>
2021-12-07 16:11:38 -0300

    encapsulamento

---
+++ object-oriented-programming/ModificadoresAcesso/Encapsulamento/Pessoa.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulamento
+{
+    public class Pessoa
+    {
+        private string nome;
+        private string genero;
+        private int idade;
+
+        public string Nome { get; }
+
+        public Pessoa(string nome)
+        {
+            this.Nome = nome;
+        }
+
+        public string Genero
+        {
+            set
+            {
+                genero = value;
+                System.Console.WriteLine(genero);
+            }
+        }
+
+        public int Idade
+        {
+            set
+            {
+                if (value < 18)
+                {
+                    System.Console.WriteLine("Necessário ser maior de 18 anos");
+                }
+                else
+                {
+                    this.idade = value;
+                }
+            }
+
+            get
+            {
+                return idade;
+            }
+        }
+    }
+}
---
+++ object-oriented-programming/ModificadoresAcesso/Encapsulamento/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Encapsulamento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa p = new Pessoa("Rafael");
+            p.Genero = "masculino";
+            p.Idade = 19;
+
+            Console.WriteLine(p.Nome);
+            Console.WriteLine(p.Idade);
+        }
+    }
+}
